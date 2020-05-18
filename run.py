e77ad36256a49d7cadffb8de3d4ed0e6de57c3b5
Vlad Bubnov <vldbubnov@gmail.com>
2020-05-18 21:01:25 +0300

    pep8

--- run.py
+++ run.py
@@ -5,8 +5,7 @@ import config as conf
 from datetime import timedelta
 
 
-
 if __name__ == '__main__':
-    
+
     sender.setup(conf.FLUENT_TAG, host=conf.FLUENT_HOST, port=conf.FLUENT_PORT)
     import logic
